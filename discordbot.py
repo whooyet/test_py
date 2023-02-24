68b1d874e69305b2e37b8001b8c8a4e0836ff642
ti_ef <rnjs5166@gmail.com>
2023-02-24 21:29:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,7 +28,7 @@ except pymysql.err.OperationalError as e:
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
