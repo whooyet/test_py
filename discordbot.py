a39acb0b1c7079ee2ffdc8dde2d509dc156095de
ti_ef <rnjs5166@gmail.com>
2023-02-25 14:18:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -113,6 +113,8 @@ async def uload(ctx):
     cursor.execute("SELECT * FROM user_info ORDER BY name")
     result = cursor.fetchall()
 
+    msg = "```md\n"
+
     for row in result:
         user_id = row[0]  # 유저의 고유 ID를 가져옴
         nickname = row[1]
